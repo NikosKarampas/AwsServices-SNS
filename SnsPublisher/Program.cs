df98daf02ca63eb1c1baabc7717572a1c6e86a29
Nikos Karampas <nikos.karampas@hotmail.com>
2023-01-27 13:31:25 +0200

    Added SnsPublisher, FindTopicAsync, PublishAsync, SqsConsumer consume from two queues

---
+++ SnsPublisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+using System.Text.Json;
+
+var customerCreated = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "nick@nickkarampas.com",
+    FullName = "Nick Karampas",
+    DateOfBirth = new DateTime(1993, 02, 28),
+    GitHubUsername = "nikoskarampas"
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customerCreated),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
+
+Console.WriteLine();
+
+
